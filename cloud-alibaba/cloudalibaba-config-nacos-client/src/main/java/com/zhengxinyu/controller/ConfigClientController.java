eb0bcd0c27f336027288f20b15dc8ce0a2acc30e
zhengxinyu <89257633+zhengxinyuzxy@users.noreply.github.com>
2022-06-18 23:05:09 +0800

    Update ConfigClientController.java
    远程dev添加

--- cloud-alibaba/cloudalibaba-config-nacos-client/src/main/java/com/zhengxinyu/controller/ConfigClientController.java
+++ cloud-alibaba/cloudalibaba-config-nacos-client/src/main/java/com/zhengxinyu/controller/ConfigClientController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConfigClientController {
 
     // 远程master添加
+    // 远程dev添加
 
     //  获取配置中心的配置数据
     @Value("${config.info}")
