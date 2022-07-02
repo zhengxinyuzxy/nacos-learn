7d86c047e20d6115a241c48f76bb0a96a98e635c
zhengxinyu <mellowzz@163.com>
2022-07-02 20:07:01 +0800

    master 20:06

--- cloud-alibaba/cloudalibaba-config-nacos-client/src/main/java/com/zhengxinyu/controller/ConfigClientController.java
+++ cloud-alibaba/cloudalibaba-config-nacos-client/src/main/java/com/zhengxinyu/controller/ConfigClientController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class ConfigClientController {
 
+    // master 20:06
+
     //  获取配置中心的配置数据
     @Value("${config.info}")
     private String configInfo;
